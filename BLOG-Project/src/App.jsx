5662616839cd4a39ffb1a7ef1b7c959ceacca27d
sagnikbera <sagnikberaofficial@gmail.com>
2024-11-05 22:34:18 +0530

    update

---
+++ BLOG-Project/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl bg-black font-bold underline text-white p-8">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
